d607037ee97b5217b9c92a9f4729ea6f66c225f9
ArtZymeri <132089036+ArtZymeri@users.noreply.github.com>
2024-01-08 00:30:54 +0100

    minor

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,14 @@ import "../styles/globals.css";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Liftoff - AI-Powered Mock Interviews",
+  title: "BM-Elektrobau",
   openGraph: {
-    title: "Liftoff - AI-Powered Mock Interviews",
-    description:
-      "Liftoff is an AI-powered mock interview platform that helps you practice for your next job interview.",
+    title: "BM-Elektrobau",
     images: [
       {
         url: "https://demo.useliftoff.com/opengraph-image",
       },
     ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Liftoff - AI-Powered Mock Interviews",
-    description:
-      "Liftoff is an AI-powered mock interview platform that helps you practice for your next job interview.",
-    images: ["https://demo.useliftoff.com/opengraph-image"],
-    creator: "@tmeyer_me",
   },
   metadataBase: new URL("https://demo.useliftoff.com"),
   themeColor: "#FFF",
